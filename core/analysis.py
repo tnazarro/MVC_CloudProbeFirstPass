24a1fe69424f181485e802f482fb3ec7ebabdef2
Tate Nazarro <tnazarro@dropletmeasurement.com>
2025-06-25 12:05:10 -0600

    Restructuring

---
+++ core/analysis.py
@@ -0,0 +1,5 @@
+"""
+Pseudo code for analysis functions.
+
+This will probably be the last part I work on today, as it is the most specific to the cloud probe data.
+"""
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# import tkinter as tk
+import tkinter as tk
 # from tkinter import ttk, filedialog, messagebox
 # import matplotlib
 # import matplotlib.pyplot as plt
